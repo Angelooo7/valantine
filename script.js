42dd56685a226a228c5f8798b56aee56936d547e
Jinni <chandurcb16@gmail.com>
2026-02-05 16:55:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ yesBtn.addEventListener("click", () => {
         mainContent.style.opacity = "1";
 
         // Update UI
-        questionText.textContent = "I knew it! Love you! 💖";
+        questionText.textContent = "I knew itttt madam jiiii…..love you toooo🥳🥳🥳";
         questionText.style.color = "#ff4081";
 
         // Update Image
